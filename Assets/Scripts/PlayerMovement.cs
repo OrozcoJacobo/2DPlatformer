951830037dd48498846a3b6b66ba39863378b8c6
JacoboOrozco <orozcojacobo1@gmail.com>
2022-05-12 21:01:52 -0500

    V1
    First draft setup for 2d platformer game

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+}
