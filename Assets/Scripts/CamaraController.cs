edc4c062c8b9480d7f0d42a5f9d8359f82ae8c30
JacoboOrozco <orozcojacobo1@gmail.com>
2022-05-18 21:23:21 -0500

    2DPlatformer

---
+++ Assets/Scripts/CamaraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CamaraController : MonoBehaviour
+{
+    //Room camera
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+    //Follow Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float camaraSpeed;
+    private float lookAhead;
+
+    private void Update()
+    {
+        //Room camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        //Follow player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * camaraSpeed);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
