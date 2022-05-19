d3d800d6ded0abbd9b70df5b04723e26e2c49347
JacoboOrozco <orozcojacobo1@gmail.com>
2022-05-19 14:04:10 -0500

    2D Platformer
    Damage machanics implemented

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOffFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -22,6 +30,7 @@ public class Health : MonoBehaviour
             //Player hurt
             anim.SetTrigger("hurt");
             //iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -39,5 +48,20 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOffFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOffFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOffFlashes * 2));
+
+        }
+        //invulnerable duration
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
